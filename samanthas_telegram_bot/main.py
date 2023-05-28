da0ff3b9fefc29fb0e0e5bbaa1d96287ed1a7fd8
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2023-05-28 17:08:53 +0300

    set httpx's logging level to WARNING to hide simple GET and POST msgs

--- samanthas_telegram_bot/main.py
+++ samanthas_telegram_bot/main.py
@@ -26,6 +26,7 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 logger = logging.getLogger(__name__)
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
 
 async def post_init(application: Application) -> None:
