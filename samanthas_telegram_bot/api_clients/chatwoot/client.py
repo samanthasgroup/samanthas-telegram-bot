97ce205b19a47a55ba37c1e41617f7d5c944ad86
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2023-07-21 13:25:37 +0300

    create conversation and send message

--- samanthas_telegram_bot/api_clients/chatwoot/client.py
+++ samanthas_telegram_bot/api_clients/chatwoot/client.py
@@ -36,8 +36,8 @@ class ChatwootClient(BaseApiClient):
         cls,
         update: Update,
         context: CUSTOM_CONTEXT_TYPES,
-    ) -> None:
-        """Creates contact in Chatwoot and starts new conversation.
+    ) -> bool:
+        """Creates contact in Chatwoot and starts new conversation. Returns `True` if successful.
 
         Steps as described in the docs:
         https://www.chatwoot.com/docs/product/channels/api/send-messages/
@@ -46,15 +46,26 @@ class ChatwootClient(BaseApiClient):
         2. Create conversation
         3. Send message to this conversation
         """
+        user_data = context.user_data
         # TODO does conversation/some params depend on role?
         # TODO do I have to store chatwoot user ID as well?
-        await cls._create_conversation(update, context)
+        source_id = await cls._create_contact(update, context)
+        conversation_id = await cls._start_conversation(update, context, source_id)
+        return await cls._send_message(
+            update,
+            context,
+            conversation_id=conversation_id,
+            text=f"New {user_data.role}: {user_data.first_name} {user_data.last_name}",
+        )
 
     @classmethod
-    async def _create_conversation(cls, update: Update, context: CUSTOM_CONTEXT_TYPES) -> str:
+    async def _create_contact(cls, update: Update, context: CUSTOM_CONTEXT_TYPES) -> str:
         """Creates new contact in Chatwoot.
 
-        Docs: https://www.chatwoot.com/developers/api/#operation/contactCreate
+         Docs:
+
+        * https://www.chatwoot.com/docs/product/channels/api/send-messages/ (overview)
+        * https://www.chatwoot.com/developers/api/#operation/contactCreate
         """
         user_data = context.user_data
 
@@ -97,3 +108,106 @@ class ChatwootClient(BaseApiClient):
         await logs(bot=context.bot, text=f"Received source_id from Chatwoot: {source_id}")
 
         return typing.cast(str, source_id)
+
+    @classmethod
+    async def _start_conversation(
+        cls, update: Update, context: CUSTOM_CONTEXT_TYPES, source_id: str
+    ) -> int:
+        """Creates new conversation in Chatwoot.
+
+        Docs:
+
+        * https://www.chatwoot.com/docs/product/channels/api/send-messages/ (overview)
+        * https://www.chatwoot.com/developers/api/#tag/Conversations/operation/newConversation
+        """
+
+        url = f"{CHATWOOT_URL_PREFIX}/conversations"
+        try:
+            _, data = await cls.post(
+                update=update,
+                context=context,
+                url=url,
+                headers=CHATWOOT_HEADERS,
+                json_data={
+                    "inbox_id": CHATWOOT_INBOX_ID,
+                    "source_id": source_id,
+                    # TODO contact_id? custom_attributes for conversation?
+                    #  team_id will definitely be very useful, not sure about assignee_id.
+                    "status": "pending",  # FIXME create enum + maybe "open" is more correct here
+                },
+                notification_params_for_status_code={
+                    httpx.codes.OK: NotificationParams(
+                        "Received data: it is expected to contain `id` for sending messages "
+                        "to this newly created Chatwoot conversation"
+                    ),
+                },
+            )
+        except BaseApiClientError as err:
+            raise ChatwootRequestError(
+                "Failed to get data with conversation ID for the new conversation from Chatwoot"
+            ) from err
+
+        try:
+            conversation_id = data["payload"]["id"]  # type:ignore
+        except KeyError as err:
+            raise ChatwootJSONParsingError(f"Could not parse Chatwoot {data=}") from err
+
+        await logs(
+            bot=context.bot,
+            text=f"Received ID of newly created Chatwoot conversation: {conversation_id}",
+        )
+
+        return typing.cast(int, conversation_id)
+
+    @classmethod
+    async def _send_message(
+        cls,
+        update: Update,
+        context: CUSTOM_CONTEXT_TYPES,
+        conversation_id: int,
+        text: str,
+    ) -> bool:
+        """Sends a message to a conversation in Chatwoot.  Returns `True` if successful.
+
+        Docs:
+
+        * https://www.chatwoot.com/docs/product/channels/api/send-messages/ (overview)
+        * https://www.chatwoot.com/developers/api/#tag/Messages/operation/create-a-new-message-in-a-conversation
+        """
+
+        url = f"{CHATWOOT_URL_PREFIX}/{conversation_id}/messages"
+        try:
+            _, data = await cls.post(
+                update=update,
+                context=context,
+                url=url,
+                headers=CHATWOOT_HEADERS,
+                json_data={
+                    "content": text,
+                    "message_type": "incoming",  # FIXME create enum
+                    # TODO content_type, content_attributes (not needed now, could be interesting)
+                },
+                notification_params_for_status_code={
+                    httpx.codes.OK: NotificationParams(
+                        "Received data: it is expected to contain `id` for sending messages "
+                        "to this newly created Chatwoot conversation"
+                    ),
+                },
+            )
+        except BaseApiClientError as err:
+            raise ChatwootRequestError(
+                f"Failed to send message to conversation with {conversation_id=}"
+            ) from err
+
+        if not data:
+            raise ChatwootJSONParsingError(
+                f"Tried to send a message to Chatwoot {conversation_id=}. "
+                "Status code is OK, but data is empty"
+            )
+
+        await logs(
+            bot=context.bot,
+            text=f"Successfully sent message to Chatwoot conversation {conversation_id}",
+        )
+
+        return True
