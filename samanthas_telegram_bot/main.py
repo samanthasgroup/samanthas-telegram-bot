4de4b6845330cc9b2d0e027eee1416238c5f17cf
Dmitry Kolomatskiy <58207913+lemontree210@users.noreply.github.com>
2023-09-02 17:13:37 +0300

    make bot_data non-persistent

--- samanthas_telegram_bot/main.py
+++ samanthas_telegram_bot/main.py
@@ -17,6 +17,7 @@ from telegram.ext import (
     CommandHandler,
     ContextTypes,
     MessageHandler,
+    PersistenceInput,
     PicklePersistence,
     TypeHandler,
     filters,
@@ -170,7 +171,13 @@ async def main() -> None:
     context_types = ContextTypes(
         context=CustomContext, user_data=UserData, chat_data=ChatData, bot_data=BotData
     )
-    persistence = PicklePersistence(filepath="bot_persistence.pickle", context_types=context_types)
+    persistence = PicklePersistence(
+        filepath="bot_persistence.pickle",
+        context_types=context_types,
+        # The bot_data being persistent will prevent it from being updated when fixing bugs
+        # or changing phrases.  The rest can be persistent.
+        store_data=PersistenceInput(bot_data=False),
+    )
 
     # Create the Application and pass it the token.
     application = (
